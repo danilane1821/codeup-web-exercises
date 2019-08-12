c30255b4b41d2a4bfedb100d7d7398bd05fa80cd
danielle lane <daniellelane1821@gmail.com>
2019-08-12 16:34:03 -0500

    done with for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,3 +8,37 @@ for(i = 1; i <= 10; i++){
 showMultiplicationTable(7);
 
 
+
+
+var x;
+
+
+function isOddOrEven() {
+    for (x=1; x <= 10; x++){
+        var random = Math.floor(Math.random() * 181) + 20;
+        if(random % 2 === 0){
+            console.log(random + " is an even number");
+        }else{
+            console.log(random + " is an odd number");
+        }
+
+    }
+}
+isOddOrEven();
+
+
+for (var i = 1; i <= 9; i++)
+
+{
+    var num = "";
+
+    for (var j = 0; j < i; j++)
+    {
+        (num = num + i);
+    }
+
+    console.log(num);
+}
+
+
+

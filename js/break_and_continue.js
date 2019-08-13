68565f039ce47f8bb0882c8ed76aebba44890071
danielle lane <daniellelane1821@gmail.com>
2019-08-13 11:17:19 -0500

    finished notes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -16,9 +16,27 @@ for(var i =1; i < 50; i+=2){
 
 
 
+//------------------------------------------------------
+// better way
 
 
 
+while (true){
+    var num = parseInt(prompt('Enter an odd number between 1 and 50'));
+    if(num % 2 !== 0 && num > 0 && num < 51){
+        break;
+    }
+}
+
+console.log("user entered" + num);
+
+for (var i = 1; i < 50; i+=2){
+   if(num === i){
+       console.log("Skip" + num);
+       continue;
+   }
+    console.log(i);
+}
 
 
 
--- js/for_loops.js
+++ js/for_loops.js
@@ -21,14 +21,14 @@ function isOddOrEven() {
         }else{
             console.log(random + " is an odd number");
         }
-
     }
 }
 isOddOrEven();
+//---------------------------
+// way 1
 
 
 for (var i = 1; i <= 9; i++)
-
 {
     var num = "";
 
@@ -39,15 +39,36 @@ for (var i = 1; i <= 9; i++)
 
     console.log(num);
 }
+//-----------------------
+// other simpler way
+
+var x = 1;
+for (var i = 1; i <= 9; i++) {
+    console.log(i * x);
+    x += "i";
+}
+//--------------------------
+//best way
+
+for (var i = 1; i <= 9; i++) {
+    console.log(string(i).repeat(i));
+}
+
+
+//--------------------------
+//next problem
+
 
 
 for (var d= 100; d >= 5; d--) {
    if(d % 5 === 0){
        console.log(d)
-   }else{
-       continue;
    }
-
 }
 
+//-------------------------
+// easier way
 
+for (var d= 100; d >= 5; d-=5) {
+    console.log(d)
+}

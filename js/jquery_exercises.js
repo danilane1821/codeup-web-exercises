6c5e5448911d22896c6f93c128744d10aebdf6a4
danielle lane <daniellelane1821@gmail.com>
2019-08-23 14:31:38 -0500

    done

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,6 +2,15 @@
 
 $(document).ready(function() {
 
-    alert('We have finished loading!')
+    // alert('We have finished loading!');
+    // alert($('#panda-disc').html());
+    // $('.codeup').css('border', 'solid 1px red');
+    $('li').css('fontsize', '20px');
+    // $('li').css('background-color', 'yellow');
+    // $('h1').css('background-color', 'yellow');
+    // $('p').css('background-color', 'yellow');
+    $('li', 'h1', 'p').css('background-color', 'yellow');
+    alert($('h1').html());
 
 });
+

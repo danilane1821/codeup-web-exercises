37c8681ae3826edbc9af5071087a83a129092cc4
danielle lane <daniellelane1821@gmail.com>
2019-08-26 09:50:57 -0500

    finished second exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,15 +2,32 @@
 
 $(document).ready(function() {
 
-    alert('We have finished loading!');
-    alert($('#panda-disc').html());
-    $('.codeup').css('border', 'solid 1px red');
-    $('li').css('font-size', '20px');
-    $('li').css('background-color', 'yellow');
-    $('h1').css('background-color', 'yellow');
-    $('p').css('background-color', 'yellow');
-    $('li, h1, p').css('background-color', 'yellow');
-    alert($('h1').html());
+    // alert('We have finished loading!');
+    // alert($('#panda-disc').html());
+    // $('.codeup').css('border', 'solid 1px red');
+    // $('li').css('font-size', '20px');
+    // $('li').css('background-color', 'yellow');
+    // $('h1').css('background-color', 'yellow');
+    // $('p').css('background-color', 'yellow');
+    // $('li, h1, p').css('background-color', 'yellow');
+    // alert($('h1').html());
+// next exercise ---------------------------------------
 
+    $('h1').click(function(){
+        $(this).css('background', 'red');
+    });
+
+    $('p').dblclick(function () {
+        $(this).css('font-size','18px')
+    });
+
+    $('li').hover (
+        function() {
+            $(this).css('color','red');
+        },
+        function (){
+        $(this).css('color','black');
+    }
+    );
 });
 

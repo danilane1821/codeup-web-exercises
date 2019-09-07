4b4ec4880ab154f29e3bb3f7f7801248ec88cf97
danielle lane <daniellelane1821@gmail.com>
2019-09-07 14:08:38 -0500

    fixed exercise

--- js/es6.js
+++ js/es6.js
@@ -53,17 +53,17 @@ const languages = [];
 
 
 // TODO: rewrite the object literal using object property shorthand
-users.push({
-  name: name,
-  email: email,
-  languages: languages
-});
+// users.push({
+//   name: name,
+//   email: email,
+//   languages: languages
+// });
 
 
 users.push({
   name,
   email,
-  languages: languages
+  languages
 });
 
 
@@ -78,55 +78,33 @@ let names = [];
 
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-
- const addUser = user => {
-   for(let user of users){
-     emails.push(user.email);
-
-   }
-
- };
- addUser();
+users.forEach(user => emails.push(user.email));
+users.forEach(user => names.push(user.name));
 
 
 
-users.forEach(function(user) {
-  return names.push(user.name);
-});
-
-
-const addName = user => {
-  for (let user of users){
-    names.push(user.name);
-  }
-};
-
-addName();
 
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach((user) => {
+users.forEach(function ({ name, email, languages }) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
 
-  const { name, email, languages } = user;
+  // const { name, email, languages } = user;
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(`${name} + 's email is ' ${email} + ${name} + ' knows ' + ${languages.join(', ')}`);
+  developers.push(`${name}'s email is ${email} ${name} knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+  for(let developer of developers) {
 
   // TODO: rewrite the assignment below to use template strings
   list += `<li>${developer}</li>`;
-});
+}
 list += '</ul>';

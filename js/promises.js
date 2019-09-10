79574c755f173d1db2eb21f03b710a13355d48bc
danielle lane <daniellelane1821@gmail.com>
2019-09-10 11:12:46 -0500

    fiished

---
+++ js/promises.js
@@ -0,0 +1,33 @@
+//
+// //my version
+// const wait = (number) => {
+//
+//     return new Promise((resolve, reject) => {
+//         setTimeout(function(){
+//         resolve();
+//      },1000)
+//     });
+//
+//
+// };
+//
+// console.log(wait(1000));
+//
+//     wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+//     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+// walk through version
+
+
+
+function wait(ms){
+    return new Promise(function (resolve) {
+        setTimeout(function(){
+            resolve();
+        }, ms)
+    });
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));

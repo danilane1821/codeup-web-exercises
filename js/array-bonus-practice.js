0dc0aa9e9b492c8a85121dfdbbac8e2ba1cb65c4
danielle lane <daniellelane1821@gmail.com>
2019-12-23 16:15:21 -0600

    more array practice

--- js/array-bonus-practice.js
+++ js/array-bonus-practice.js
@@ -1,6 +1,6 @@
 
-var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
-var emptyArr = [];
+let fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+let emptyArr = [];
 
 
 function allIndexesOf (fruit, value) {
@@ -11,3 +11,13 @@ function allIndexesOf (fruit, value) {
 console.log(allIndexesOf(fruits, "apple")); //should return the array [0, 3]
 console.log(allIndexesOf(fruits, "guava"));//should return the array []
 console.log(allIndexesOf(fruits, "pineapple")); //should return [4]
+
+
+function firstLast6(nums){
+    if(nums[0] === 6 ||nums[nums.length-1] === 6){
+        return true;
+    }else{
+        return false;
+    }
+}
+

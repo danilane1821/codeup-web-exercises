03dcf9ed9ae1c0a6439d7a340c998f52bdcebeb4
danielle lane <daniellelane1821@gmail.com>
2019-12-23 16:25:26 -0600

    more array practice

--- js/array-bonus-practice.js
+++ js/array-bonus-practice.js
@@ -13,6 +13,14 @@ console.log(allIndexesOf(fruits, "guava"));//should return the array []
 console.log(allIndexesOf(fruits, "pineapple")); //should return [4]
 
 
+
+// Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+//     Examples
+// firstLast6([1, 2, 6]) → true
+// firstLast6([6, 1, 2, 3]) → true
+// firstLast6([13, 6, 1, 2, 3]) → false
+
+
 function firstLast6(nums){
     if(nums[0] === 6 ||nums[nums.length-1] === 6){
         return true;
@@ -21,3 +29,28 @@ function firstLast6(nums){
     }
 }
 
+// or
+
+function firstLast6(nums){
+    return nums[0] === 6 || nums[nums.length-1] === 6
+
+}
+
+
+// Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+
+//     Examples
+// sameFirstLast([1, 2, 3]) → false
+// sameFirstLast([1, 2, 3, 1]) → true
+// sameFirstLast([1, 2, 1]) → true
+
+function sameFirstLast(nums){
+    return nums[0] === nums[nums.length -1] && nums.length > 0;
+}
+
+
+// Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+function makePi(){
+    return [3,1,4];
+}
